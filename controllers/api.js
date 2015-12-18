e3c04b3dd62e81e5e438ed5af79b0e4361005ff4
Wale O <wale@screenverve.com>
2015-12-18 11:13:34 -0800

    set up mongo/mongoose call for finding One API

--- controllers/api.js
+++ controllers/api.js
@@ -23,9 +23,9 @@ router.route('/')
 router.route('/:id')
   .get(function getOneApi(req, res){
     console.log('get one API - ID #' + req.params.id);
-    res.send({
-      success: 'true',
-      method: 'GET'
+    Api.findOne({_id: req.params.id}, function sendFoundApi(err, api){
+      if(err) return res.status(500).send(err);
+      res.send(api);
     });
   })
   .put(function putOneApi(req, res){
